df23829e9e7756dfa961db8b960185ec369df858
aamirpinger <aamirpinger@yahoo.com>
2019-03-12 13:02:42 +0500

    Variable Hoisting Example

---
+++ variableHoistingExample.js
@@ -0,0 +1,30 @@
+//VARIABLE HOISTING
+// Try any part one at a time
+
+var myname;         // Declare Variable
+myname = 'Aamir';   // Initialize variable
+console.log(myname) // result: Aamir
+
+
+
+/*********************************** */
+
+// var myname;          // Declare Variable
+// console.log(myname)  // result: Undefined
+// myname = 'Aamir';    // Initialize variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: Undefined
+// myname = 'Aamir';    // Initialize variable
+// var myname;          // Declare Variable
+
+
+
+/*********************************** */
+
+// console.log(myname)  // result: ERROR: myname is not defined
+//myname = 'Aamir';     // Initialize variable
+
