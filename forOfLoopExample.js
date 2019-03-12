36a02bc217a8a7a2efde415782e4a973a332e483
aamirpinger <aamirpinger@yahoo.com>
2019-03-12 13:06:59 +0500

    For OF loop example code

---
+++ forOfLoopExample.js
@@ -0,0 +1,27 @@
+// "For of" loop
+
+
+const digits = ["a", "b", "c", "d", "e", "f", "g", "h"]
+const myname = "aamir"
+
+for (const index of digits) {
+   console.log(index);
+}
+for (const index of myname) {
+   console.log(index);
+}
+
+//Result:
+// a
+// b
+// c
+// d
+// e
+// f
+// g
+// h
+// a
+// a
+// m
+// i
+// r
