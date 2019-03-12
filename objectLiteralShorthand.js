fa6825b764b7ee03d0cfe37d913cb55cb721f196
aamirpinger <aamirpinger@yahoo.com>
2019-03-12 13:05:52 +0500

    Object Literal Shorthand example code

---
+++ objectLiteralShorthand.js
@@ -0,0 +1,34 @@
+//OBJECT LITERAL SHORTHAND
+
+
+const firstName = "Aamir"
+let lastName = "Pinger"
+var degrees = 2
+
+/************************************************** */
+// Try any part one at a time
+
+
+let myObject = {
+    firstName: firstName,
+    lastName: lastName,
+    degrees: degrees
+}
+console.log(myObject)  //RESULT: {firstName: "Aamir", lastName: "Pinger", degrees: 2}
+
+
+
+
+
+/************************************************** */
+
+// const firstName = "Aamir"
+// let lastName = "Pinger"
+// var degrees = 2
+
+// let myObject = {
+//     firstName,
+//     lastName,
+//     degrees
+// }
+// console.log(myObject)  //RESULT: {firstName: "Aamir", lastName: "Pinger", degrees: 2}
