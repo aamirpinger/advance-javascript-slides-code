c4e7ddb1344affde9592c8d91766bd1c9b05e946
aamirpinger <aamirpinger@yahoo.com>
2019-03-12 13:07:33 +0500

    Destructuring Example code

---
+++ destructuringExample.js
@@ -0,0 +1,101 @@
+//DESTRUCTURING ARRAY
+
+/************************************************** */
+// Try any part one at a time
+
+
+let myArray = ["Aamir","Pinger",2]
+
+let [firstName, lastName, degrees] = myArray
+
+console.log(firstName)
+console.log(lastName)
+console.log(degrees)
+
+let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees.`
+
+console.log(sentence)
+// RESULT:
+// Aamir
+// Pinger
+// 2
+// Aamir Pinger has 2 Masters degrees.
+
+
+
+
+// /************************************************** */
+// let myArray = ["Aamir","Pinger",2]
+
+// let [firstName, lastName] = myArray
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees.`
+
+// console.log(sentence)
+// // RESULT:
+// // Aamir
+// // Pinger
+// // degrees is not defined
+
+
+
+
+
+
+
+// /************************************************** */
+//DESTRUCTURING OBJECT
+
+
+// let myObject = {firstName: "Aamir", lastName: "Pinger", degrees: 2}
+
+// console.log(myObject.firstName)
+// console.log(myObject.lastName)
+// console.log(myObject.degrees)
+
+// let {firstName, lastName, degrees} = myObject
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees`
+
+// console.log(sentence)
+// Aamir
+// Pinger
+// 2
+// Aamir
+// Pinger
+// 2
+// Aamir Pinger has 2 Masters degrees.
+
+
+
+
+
+
+
+// /************************************************** */
+
+
+// let myObject = {firstName: "Aamir", lastName: "Pinger", degrees: 2}
+
+// console.log(myObject.firstName)
+// console.log(myObject.lastName)
+// console.log(myObject.degrees)
+
+// let {firstName, lastName} = myObject
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(degrees)
+
+// let sentence = `${firstName} ${lastName} has ${degrees} Masters degrees`
+
+// console.log(sentence)
+
