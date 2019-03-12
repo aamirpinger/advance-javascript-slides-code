0dd160e47bced474adc2c7072cf5505651dee46a
aamirpinger <aamirpinger@yahoo.com>
2019-03-12 13:08:09 +0500

    CONST example code

---
+++ constExample.js
@@ -0,0 +1,38 @@
+// CONST
+
+// Excellent article on LET, CONST, Variable
+// https://andy-carter.com/blog/variable-scope-in-modern-javascript
+
+
+
+/************************************************** */
+// Try any part one at a time
+const myname = 'Aamir'; // Declare and Initialize variable
+console.log(myname)     // result: Aamir
+
+
+
+
+
+/************************************************** */
+// const myname        // Declare variable
+// myname = 'Aamir';   // Initialize variable
+// console.log(myname) // RESULT: Missing initializer in const declaration
+
+
+
+
+/************************************************** */
+// myname = 'Aamir';   // Initialize variable
+// console.log(myname) // RESULT: Missing initializer in const declaration
+// const myname        // Declare variable
+
+
+
+
+
+/************************************************** */
+// myname = 'Aamir';    // Initialize variable
+// const myname         // Declare variable
+// console.log(myname)  // RESULT: Missing initializer in const declaration
+
