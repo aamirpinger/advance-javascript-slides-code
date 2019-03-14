2224f78056aa221216eb9181200b220cd4c4969a
aamirpinger <aamirpinger@yahoo.com>
2019-03-14 11:46:51 +0500

    Object.keys Example code

---
+++ ObjectDotkeysExample.js
@@ -0,0 +1,13 @@
+//Object.keys
+
+let obj = {
+    fname: "Aamir",
+    lname: "Pinger",
+    degrees: 2,
+}
+
+let newArray = Object.keys(obj)
+
+console.log(newArray)  //RESULT : ["fname", "lname", "degrees"]
+
+
