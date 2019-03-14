59b0e82134b4df79417e7b76b3bc9b832f937b44
aamirpinger <aamirpinger@yahoo.com>
2019-03-14 11:46:35 +0500

    Object.values Example code

---
+++ ObjectDotValuesExample.js
@@ -0,0 +1,11 @@
+// Object.values
+
+let obj = {
+    fname: "Aamir",
+    lname: "Pinger",
+    degrees: 2,
+}
+
+let newArray = Object.values(obj)
+
+console.log(newArray)   // RESULT: ["Aamir", "Pinger", 2]
